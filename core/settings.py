0ad58e2a8b08d95564fb877fab8ce0e3b11924b2
Shanmuga-raj <rajmc222@gmail.com>
2022-03-08 18:12:36 +0530

    Added heroku postgres database connection.

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
-from dj_database_url import parse
+import dj_database_url
 
 load_dotenv(find_dotenv())
 
@@ -87,6 +87,9 @@ DATABASES = {
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Only Logged in user's can able to access (For all API's):
 REST_FRAMEWORK = {
